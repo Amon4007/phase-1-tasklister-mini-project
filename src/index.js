8c98bfc02c02a9675491f343dfb8ed2c09fd50af
amon <amonlimo4@gmail.com>
2024-10-06 13:28:04 +0300

    add style.css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+
+    const newTaskDescription = document.getElementById("new-task-description").value;
+
+    if (newTaskDescription === "") return;
+
+    const newTaskItem = document.createElement("li");
+    newTaskItem.textContent = newTaskDescription;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.style.marginLeft = "10px";
+
+
+    deleteButton.addEventListener("click", () => {
+      newTaskItem.remove();
+    });
+
+
+    newTaskItem.appendChild(deleteButton);
+
+
+    taskList.appendChild(newTaskItem);
+
+
+    event.target.reset();
+  });
 });
+
